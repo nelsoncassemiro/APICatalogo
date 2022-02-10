d1c262f875b417cf48044554bba3f6ad242e6b96
nelson de sousa cassemiro <nelsoncassemiro@gmail.com>
2022-02-09 23:02:55 -0300

    Finalizando criação das models e populando as tabelas

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Descricao { get; set; }
+        [Required]
+        public decimal Preco { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
+
+    }
+}
--- APICatalogo/Startup.cs
+++ APICatalogo/Startup.cs
@@ -1,7 +1,9 @@
+using APICatalogo.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,9 @@ namespace APICatalogo
         public void ConfigureServices(IServiceCollection services)
         {
 
+            
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CatalogoConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
